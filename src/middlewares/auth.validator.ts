a2ae540fed6906e725e25b698a67f78d64c4b9d6
HardUsername-123 <anthonycrausus14.darkcode@gmail.com>
2025-11-24 06:25:17 +0800

    feat: add new roles to registration validation; include 'umsa', 'sgo', and 'kahayag' in valid roles list

--- src/middlewares/auth.validator.ts
+++ src/middlewares/auth.validator.ts
@@ -69,6 +69,9 @@ export const validateRegister = (
       "library",
       "tailoring",
       "guidance",
+      "umsa",
+      "sgo",
+      "kahayag",
     ];
     if (!validRoles.includes(role)) {
       res.status(400).json({
