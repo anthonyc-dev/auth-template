f98612f26ef833ba1ee6663a420895f09ee60f46
HardUsername-123 <anthonycrausus14.darkcode@gmail.com>
2025-11-23 08:20:13 +0800

    refactor: remove authentication middleware from user profile and password update routes; streamline route definitions for clarity

--- src/routes/intigration.route.ts
+++ src/routes/intigration.route.ts
@@ -4,7 +4,6 @@ import {
   updateClearingOfficerPassword,
   updateClearingOfficerProfile,
 } from "../controllers/intigration.controller";
-import { authenticateToken } from "../middlewares/authentication";
 import { upload } from "../config/multer";
 
 const router = Router();
@@ -12,20 +11,11 @@ const router = Router();
 // PUT /clearingOfficer/update/:id
 router.put(
   "/updateUserProfile/:id",
-  authenticateToken,
   upload.single("profileImage"),
   updateClearingOfficerProfile
 );
-router.put(
-  "/updateUserPassword/:id",
-  authenticateToken,
-  updateClearingOfficerPassword
-);
+router.put("/updateUserPassword/:id", updateClearingOfficerPassword);
 
-router.get(
-  "/getAllStudentBySchoolId/:schoolId",
-  authenticateToken,
-  getAllStudentBySchoolId
-);
+router.get("/getAllStudentBySchoolId/:schoolId", getAllStudentBySchoolId);
 
 export default router;
